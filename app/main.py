00f0f29ef3dfff699a825e104ad70ec2609b5390
Sofia G S Pedrini <sofiagspedrini@gmail.com>
2025-06-25 17:51:21 -0300

    Updated requirements to include main.py packages

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
 from tensorflow.keras.models import load_model
-import numpy as np
 from app.utils.preprocessing import preprocessing
 
 app = Flask(__name__)
